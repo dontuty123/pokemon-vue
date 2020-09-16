c1cb208e28f2fc3c562c2ee9b46d952ac61b4a52
lamlt <lamlt@runsystem.net>
2020-09-16 13:59:10 +0700

    update foler er-system for review

--- 3.Js/vue/er-system/src/core/router/index.js
+++ 3.Js/vue/er-system/src/core/router/index.js
@@ -7,10 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/Login/index.vue')
+    component: () => import('@/views/Login/index.vue')
   }
 ]
 
