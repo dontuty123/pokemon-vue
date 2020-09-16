c8f5f7843e0fa3518e1dbc1eaa2af97d9856eae3
Tri Nguyen <trinc@runsystem.net>
2020-09-16 18:05:24 +0700

    call/api

--- 3.Js/react/tri/ER/demo/src/components/button.js
+++ 3.Js/react/tri/ER/demo/src/components/button.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './css.css';
 
 class Button extends React.Component {
-    constructor(props){
-        super();
-    }
+
     render() {
         return (<input className="button" {...this.props} type="button" />)
     }
