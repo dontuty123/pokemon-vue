2158605b8116f1c97a286c0e62d5542021a51fea
duytttt <duyttt@runsystem.net>
2020-09-30 16:15:23 +0700

    update code

--- 3.Js/react/ER-System/source/src/components/Form/index.tsx
+++ 3.Js/react/ER-System/source/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './form-style.scss';
 
 interface Props {
@@ -12,7 +12,7 @@ interface State {
     dataSource: any;
 }
 
-class Form extends React.Component<Props, State> {
+class Form extends Component<Props, State> {
 
     state: State = {
         dataSource: {},
@@ -20,18 +20,18 @@ class Form extends React.Component<Props, State> {
 
     componentDidUpdate = (prevProps: Props, prevState: State) => {
         if (JSON.stringify(prevProps.dataSource) !== JSON.stringify(this.props.dataSource)) {
-            this.setState({ dataSource: this.props.dataSource})
+            this.setState({dataSource: this.props.dataSource})
         }
     }
 
     onChangeData = (e: React.FormEvent<HTMLInputElement | HTMLSelectElement>) => {
         const {dataSource} = this.state;
         const value = e.currentTarget.value,
-             name = e.currentTarget.name,
-             data = {
-                 ...dataSource,
-                 [name]: value
-                };
+            name = e.currentTarget.name,
+            data = {
+                ...dataSource,
+                [name]: value
+            };
 
         this.setState({dataSource: data}, () => this.props.onChange(data))
     }
@@ -39,55 +39,55 @@ class Form extends React.Component<Props, State> {
     renderInput = (item: any) => {
         const {dataSource} = this.state;
 
-        return <input 
-                    type="text"
-                    name={item.idIndex}
-                    maxLength={item.maxLength} 
-                    className="text-form" 
-                    onChange={(e) => this.onChangeData(e)} 
-                    value={dataSource[item.idIndex]}
-                />
+        return <input
+            type="text"
+            name={item.idIndex}
+            maxLength={item.maxLength}
+            className="text-form"
+            onChange={(e) => this.onChangeData(e)}
+            value={dataSource[item.idIndex]}
+        />
     }
 
     renderSelect = (item: any) => {
         const {dataSource} = this.state;
         const options: Array<any> = item.options;
 
-        return <select 
-                    className="select-form" 
-                    onChange={(e) => this.onChangeData(e)} 
-                    name={item.idIndex}
-                    value={dataSource[item.idIndex]}
-                >
-                    {options.map(ele => (<option value={ele.value}>{ele.title}</option>))}
-                </select>
+        return <select
+            className="select-form"
+            onChange={(e) => this.onChangeData(e)}
+            name={item.idIndex}
+            value={dataSource[item.idIndex]}
+        >
+            {options.map(ele => (<option value={ele.value}>{ele.title}</option>))}
+        </select>
     }
 
     renderFile = (item: any) => {
 
-        return <input 
-                    type="file"
-                    name={item.idIndex}
-                    maxLength={item.maxLength} 
-                    onChange={(e) => this.onChangeData(e)} 
-                    className="file-form"
-                />
+        return <input
+            type="file"
+            name={item.idIndex}
+            maxLength={item.maxLength}
+            onChange={(e) => this.onChangeData(e)}
+            className="file-form"
+        />
     }
 
     renderCheckbox = (item: any) => {
         const {dataSource} = this.state;
         const checked = dataSource[item.idIndex] === "1";
-        
-        return <input 
-                    type="checkbox"
-                    name={item.idIndex}
-                    onChange={(e) => this.onChangeData(e)} 
-                    checked={checked}
-                />
+
+        return <input
+            type="checkbox"
+            name={item.idIndex}
+            onChange={(e) => this.onChangeData(e)}
+            checked={checked}
+        />
     }
 
     renderForm = () => {
-        const { columns } = this.props;
+        const {columns} = this.props;
         let renderType: any;
         return columns.map(item => {
             switch (item.type) {
@@ -106,14 +106,14 @@ class Form extends React.Component<Props, State> {
             }
 
             return <tr key={item.idForm + "_" + item.idIndex}>
-                        <td>
-                            <span>{item.title}</span>
-                            {item.validate ? <span className="required">*</span> : null}
-                        </td>
-                        <td className="field-form">
-                            {renderType}
-                        </td>
-                    </tr>
+                <td>
+                    <span>{item.title}</span>
+                    {item.validate ? <span className="required">*</span> : null}
+                </td>
+                <td className="field-form">
+                    {renderType}
+                </td>
+            </tr>
         })
     }
 
@@ -122,7 +122,7 @@ class Form extends React.Component<Props, State> {
             <div className="form-table">
                 <table>
                     <tbody>
-                        {this.renderForm()}
+                    {this.renderForm()}
                     </tbody>
                 </table>
             </div>
